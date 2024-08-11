2098b3f6aa08a25f24e7b15dc6cc72ebbf9914e9
timmn <timmmmnn@gmail.com>
2024-08-11 21:43:03 +0200

    Stuff

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,4 +1,10 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
+vim.opt.tabstop = 4 -- size of a hard tabstop (ts).
+vim.opt.shiftwidth = 4 -- size of an indentation (sw).
+vim.opt.expandtab = true -- always uses spaces instead of tab characters (et).
+vim.opt.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
