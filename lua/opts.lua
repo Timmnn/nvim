55dab5d6907663630dfd430500f55654b067584d
timmn <timmmmnn@gmail.com>
2024-08-26 15:40:37 +0200

    Changed Tabsize

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,9 +1,9 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 
-vim.opt.tabstop = 4 -- size of a hard tabstop (ts).
-vim.opt.shiftwidth = 4 -- size of an indentation (sw).
+vim.opt.tabstop = 2      -- size of a hard tabstop (ts).
+vim.opt.shiftwidth = 2   -- size of an indentation (sw).
 vim.opt.expandtab = true -- always uses spaces instead of tab characters (et).
-vim.opt.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
+vim.opt.softtabstop = 2  -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
 
 vim.opt.termguicolors = true
 
