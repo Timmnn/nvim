d7a9592fc4d68ab2755dcf3b9d45fa0db43ca7e3
Timm Nicolaizik <timmmmnn@gmail.com>
2025-03-18 15:02:06 +0100

    Added usefull keybinds

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Go to lower window" })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "Go to upper window" })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Go to right window" })
+
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
